a463c185de84a540b14958e8b86ef24f22216a7b
hyjoshi14 <hyjoshi_14@hotmail.com>
2016-09-22 02:54:36 +0530

    Update with IID attribute

--- xml_parse.py
+++ xml_parse.py
@@ -1,6 +1,7 @@
 import os, pprint
 from bs4 import BeautifulSoup as bs
 
+## Solution to Quiz:Extracting Data
 def main_bs():
     print os.getcwd()
     f_xml = open('exampleResearchArticle.xml','r')
@@ -20,5 +21,28 @@ def main_bs():
         out.append(author_dict)
     return out
 
+## Solution to Quiz:Handling Attributes
+def main_bs2():
+    print os.getcwd()
+    f_xml = open('exampleResearchArticle.xml','r')
+    soup = bs(f_xml.read(),'lxml')
+    authors = soup.findAll('au')
+    out = []
+    for author in authors:
+        author_dict = {'insr':[]}
+        for child in author.childGenerator():
+            if child == u'\n':
+                    continue
+            elif child.name == 'insr':
+                author_dict['insr'] += [child['iid'].strip()]
+            else:
+                print child.name,':', child.get_text().strip()
+                author_dict[child.name] = child.get_text().strip()
+        pprint.pprint(author_dict)
+        print
+        out.append(author_dict)
+    return out
+
 if __name__ == '__main__':
     pprint.pprint(main_bs())
+    pprint.pprint(main_bs2())
